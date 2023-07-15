9aaa70123bd477fe2bda51b0321a4f12336dbc37
이한주 <dlgkswn3124@naver.com>
2023-07-15 22:44:30 +0900

    feat:change pattern

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+const output = {
+    hello: (req, res)=>{
+        res.render("home/index");
+    },  
+    login: (req,res)=>{
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login: (req,res)=>{
+        console.log(req.body);
+    },
+};
+
+
+module.exports ={
+    output,
+    process,
+};
