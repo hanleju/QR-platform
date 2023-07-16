9187b6d26bb7c819854e7a60b1d51d8dea5405b8
이한주 <dlgkswn3124@naver.com>
2023-07-16 12:16:50 +0900

    feat:login기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
-const UserStorage = require("../../models/UserStorage");
 
+const User = require("../../models/User");
 
 
 const output = {
@@ -12,30 +12,13 @@ const output = {
     },
 };
 
-
-
 const process = {
-    login: (req,res)=>{
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id");
-
-        const response = {};
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx]===psword){
-                response.success = true;
-                return res.json();
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인 실패"
+    login: (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
-
 
 module.exports ={
     output,
