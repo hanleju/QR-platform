47c594e1b91dd00b39ffe6be67439b5168948eff
이한주 <dlgkswn3124@naver.com>
2023-07-16 22:37:05 +0900

    feat:회원가입 fs모듈 이용

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,6 +3,7 @@
 const fs = require("fs").promises;
 
 class UserStorage{
+
     static #getUserInfo(data,id){
         const users = (JSON.parse(data));
         const idx = users.id.indexOf(id);
@@ -14,9 +15,10 @@ class UserStorage{
         console.log(userInfo);
         return userInfo;
     }
-    
-    static getUsers(...fields){
-        // const users = this.#users
+
+    static #getUsers(data,isAll,fields){
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers,field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -25,6 +27,16 @@ class UserStorage{
         }, {});
         return newUsers;
     }
+
+    static getUsers(isAll,...fields){
+        return fs
+        .readFile("./src/database/users.json") 
+        .then((data)=>{
+            return this.#getUsers(data,isAll,fields)
+        })
+        .catch(console.error);
+    }
+
     static getUserInfo(id){
         return fs.readFile("./src/database/users.json") 
             .then((data)=>{
@@ -33,11 +45,16 @@ class UserStorage{
             .catch(console.error);
     }
 
-    static save(userInfo){
-        // const users = this.#users;
-        users.id.push(userInfo.id);
+    static async save(userInfo){
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)){
+            throw "이미 존재하는 아이디입니다.";
+        }
+    users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
+        fs.writeFile("./src/database/users.json", JSON.stringify(users));
+        return {success:true};
     }
 }
 
